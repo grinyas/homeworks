7b17d655db6e3542d87c4e3501c287aa09b8f27a
grinyas <grinyas00@gmail.com>
2016-10-06 11:10:16 +0300

    Exercise #2

--- arrays_loops_tasks/2/index.php
+++ arrays_loops_tasks/2/index.php
@@ -5,3 +5,14 @@
  * Date: 06.10.2016
  * Time: 11:03
  */
+
+$array = [1, 20, 15, 17, 24, 35];
+print_r($array);
+
+$sum = 0;
+
+foreach ($array as $item) {
+    $sum += $item;
+}
+
+echo "<br>Total: " . $sum;
