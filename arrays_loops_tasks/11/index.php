33a90b879c09a71614ef48f58a7d923a94feb793
grinyas <grinyas00@gmail.com>
2016-10-06 14:09:39 +0300

    Exercise #11

---
+++ arrays_loops_tasks/11/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agrigor
+ * Date: 06.10.2016
+ * Time: 14:05
+ */
+
+for ($i = 0; $i <= 100; $i++) {
+    if (!($i % 2) ){
+        echo $i . "<br>";
+    }
+}
