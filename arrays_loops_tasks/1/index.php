f25505e173c322e9d8de02d042ff9fd6a5e91a3a
grinyas <grinyas00@gmail.com>
2016-10-06 11:04:30 +0300

    Exercise #1

---
+++ arrays_loops_tasks/1/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agrigor
+ * Date: 06.10.2016
+ * Time: 10:53
+ */
+
+$array = ['html', 'css', 'php', 'js', 'jq'];
+
+foreach ($array as $item){
+    echo $item . '<br>';
+}
