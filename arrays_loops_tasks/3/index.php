3e36f3274d6f946e4e8f685bc7f094b026f2f1af
grinyas <grinyas00@gmail.com>
2016-10-06 11:22:07 +0300

    Exercise #3

--- arrays_loops_tasks/3/index.php
+++ arrays_loops_tasks/3/index.php
@@ -6,3 +6,13 @@
  * Time: 11:10
  */
 
+
+$array = [26, 17, 136, 12, 79, 15];
+print_r($array);
+
+$result = 0;
+foreach ($array as $item) {
+    $result += $item * $item;
+}
+
+echo "<br>Total: " . $result;
