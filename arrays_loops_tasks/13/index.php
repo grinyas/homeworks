e7533688114b4ca05c34ef42d91f8924f518d540
grinyas <grinyas00@gmail.com>
2016-10-06 14:29:29 +0300

    Exercise #13

---
+++ arrays_loops_tasks/13/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agrigor
+ * Date: 06.10.2016
+ * Time: 14:21
+ */
+
+
+for ($i = 1; $i < 10; $i++) {
+    echo "<p>";
+
+    for ($j = 1; $j < 10; $j++) {
+        echo "$i * $j =" . $i * $j . "<br>";
+    }
+}
