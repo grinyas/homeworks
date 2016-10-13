1b707e9980d37b3488bd6996d8f03a3547faa644
grinyas <grinyas00@gmail.com>
2016-10-13 13:56:15 +0300

    Exercise #7

---
+++ functions_forms_tasks/7.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>#7</title>
+</head>
+<body>
+
+<p>Коментарии</p>
+<textarea name="comments"></textarea>
+</body>
+</html>
+
+<?php
+
+    function getComments(){
+
+
+    };
+
+
+?>
