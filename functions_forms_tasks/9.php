8806ae2098a12deb9c873623f16eff12bffca762
grinyas <grinyas00@gmail.com>
2016-10-15 21:58:51 +0300

    Reverse string

--- functions_forms_tasks/9.php
+++ functions_forms_tasks/9.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: agrigor
- * Date: 15.10.2016
- * Time: 21:27
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>№9</title>
+</head>
+<body>
+
+<form method="post">
+    <label for="string">Введите строку:</label>
+    <input type="text" name="string" id="string">
+    <input type="submit" value="Перевернуть">
+    <div class="result">
+        <?php (empty($_POST['string'])) ? die("Пусто!") : print_r(strrev($_POST['string']))?>
+    </div>
+</form>
+
+
+</body>
+</html>
+
