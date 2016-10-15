46bd976bf53a4fca0f9a4fcca52e2790ba01bca0
grinyas <grinyas00@gmail.com>
2016-10-16 00:16:47 +0300

    Exercise #11

--- functions_forms_tasks/11.php
+++ functions_forms_tasks/11.php
@@ -10,15 +10,35 @@
 <form method="post">
     <div>
         <p><label for="text">Введите текст:</label></p>
-        <textarea type="text" name="text" id="text" cols="50" rows="10"></textarea>
+        <textarea type="text" name="text" id="text" cols="50" rows="10" >а васька слушает да ест. а воз и ныне там. а вы друзья как ни садитесь, все в музыканты не годитесь. а король-то — голый.а ларчик просто открывался. а там хоть трава не расти</textarea>
         <p><input type="submit" value="Форматировать"></p>
     </div>
     <div class="result">
         <p>Форматированый текст:</p>
-        <pre>
+
         <?php getFormatedText() ?>
-            </pre>
+
     </div>
 </form>
 </body>
 </html>
+<?php
+
+function getFormatedText()
+{
+    if (empty($_POST['text'])) die("Пусто!");
+
+    $text = $_POST['text'];
+    $text = mb_split('\.', $text);
+
+    array_walk($text, function (&$value) {
+        $firstL = mb_strtoupper(substr($value, 0, 2), "utf-8");
+        $value[1] = $firstL[1];
+    });
+
+
+    $text = str_replace(".", "", $text);
+    print_r(implode(". ", $text) . ".");
+}
+
+?>
