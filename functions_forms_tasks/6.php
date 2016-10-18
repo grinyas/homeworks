185f0fb27984f4fbd06dec5606a46687875631cf
grinyas <grinyas00@gmail.com>
2016-10-18 11:18:00 +0300

    Exercise #6

--- functions_forms_tasks/6.php
+++ functions_forms_tasks/6.php
@@ -7,42 +7,83 @@
 <body>
 <form method="post" enctype="multipart/form-data">
     <p>Загрузить картинки</p>
-    <input type="file" multiple name="pics[]">
-    <input type="submit">
-
+    <input type="file"  name="pics[]" multiple>
+    <p><input type="submit"></p>
 </form>
-</body>
-</html>
+<?php renderGallery('gallery'); ?>
 
 <?php
 
-$dir = 'gallery';
+$galleryDir = 'gallery';
 
-$allowedMimes =[
-        'image/png',
-        'image/jpg',
-        'image/gif',
+$allowedMimes = [
+    'image/png',
+    'image/jpg',
+    'image/gif',
 ];
 
-if(!empty($_FILES))
+if (!empty($_FILES)) {
     $files = [];
-
     $upload = $_FILES['pics'];
-    foreach ($upload as $key => $name){
-        $tmpName = $upload['name'][$key];
+
+
+    foreach ($upload['name'] as $key => $name) {
+
+        $tmpName = $upload['tmp_name'][$key];
         $info = new finfo(FILEINFO_MIME_TYPE);
         $mime = $info->file($tmpName);
         $extension = pathinfo($name, PATHINFO_EXTENSION);
 
-        if(!in_array($mime, $allowedMimes))
+        if (!in_array($mime, $allowedMimes))
             continue;
 
-        $files = [
-            'tmpNmae' => $tmpName,
-            'destination' => md5($tmpName) . $extension
+        $files[] = [
+            'tmpName' => $tmpName,
+            'destination' => md5($tmpName). "." . $extension
         ];
     }
 
+    saveImages($files);
+}
 
+function saveImages(array $images)
+{
+//    echo "<pre>";
+//var_dump($images);
+//    echo "</pre>";
+    global $galleryDir;
+
+    if (!is_dir($galleryDir)) {
+        mkdir($galleryDir);
+    }
+
+    $cwd = getcwd();
+
+    chdir($galleryDir);
+    foreach ($images as &$image) {
+        move_uploaded_file($image['tmpName'], $image['destination']);
+    }
+    chdir($cwd);
+}
+
+function renderGallery($galleryDir)
+{
+    if (!is_dir($galleryDir)) {
+        echo "No image render";
+        return;
+    }
+
+    $cwd = getcwd();
+    chdir($galleryDir);
+    $images = glob('*.png');
+
+    foreach ($images as $image) {
+        echo "<img src=\"$galleryDir/$image\" width=\"125px\">";
+    }
+    chdir($cwd);
+    unset($images);
+}
 
 ?>
+</body>
+</html>
